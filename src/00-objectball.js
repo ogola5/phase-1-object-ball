d5aaa6b8ac0262d6464ff07345e99bf1363a8b03
ogola5 <ogolaevance5@gmail.com>
2022-08-23 10:34:49 +0300

    completed assignment

---
+++ src/00-objectball.js
@@ -0,0 +1,70 @@
+function gameObject(){
+
+    let  g = {
+    home:{
+        teamName : "Brooklyn Nets",
+        colors: [],
+        playesrs :{
+            "Alan Anderson": {  
+                number: 0,
+                shoe: 16,
+                points:22,
+                rebounds:12,
+                assists:12,
+                steals:3,
+                blocks:1,
+                slamDunks:1,
+                
+
+            },
+        }
+        
+        
+    }
+    }
+
+
+
+
+
+
+
+return g
+
+}
+
+
+
+
+let  g = {
+    home:{
+        teamName : "Brooklyn Nets",
+        colors: [],
+        playesrs :{
+            "Alan Anderson": {  
+                number: 0,
+                shoe: 16,
+                points:22,
+                rebounds:12,
+                assists:12,
+                steals:3,
+                blocks:1,
+                slamDunks:1,
+                
+
+            },
+        },
+        
+        
+    },
+    }
+
+    function numPointsScored (name){
+    
+
+        if(name === g.home['playesrs']){
+            return g.home['playesrs'].points
+        }
+       
+
+}
